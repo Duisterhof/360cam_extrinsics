eee55e1f6ab4df681256a8e97b4279266764489b
Duisterhof <b.p.duisterhof@student.tudelft.nl>
2022-12-12 11:05:39 -0500

    first output

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from extract_features import FeatureExtractor
 from pixel2ray import Pixel2Ray
+from ransac_E import Essential_Optimizer
+from pytransform3d.transform_manager import TransformManager
+from pytransform3d import transformations as pt
+import matplotlib.pyplot as plt
 
 cam0_path = 'data/cam0/'
 cam1_path = 'data/cam1/'
@@ -10,8 +14,30 @@ cams.load_cams()
 
 extractor = FeatureExtractor(cam0_path,cam1_path)
 extractor.extract_features()
+# extractor.vis_features()
 
 rays0 = cams.cams[0].pixel_2_ray(extractor.cam0_coords)[0].numpy().reshape((3,-1))
 rays1 = cams.cams[1].pixel_2_ray(extractor.cam1_coords)[0].numpy().reshape((3,-1))
 
-# extractor.vis_features()
+print(rays0.shape)
+print(rays1.shape)
+
+E_Class = Essential_Optimizer(rays0,rays1)
+E_Class.compute_system()
+E_Class.full_lstsq()
+E_Class.extract_motion()
+
+print(E_Class.E)
+print(E_Class.R)
+print(E_Class.t)
+
+cam0_2_cam1 = pt.transform_from(E_Class.R,E_Class.t)
+
+tm = TransformManager()
+tm.add_transform("Cam0","Cam1",cam0_2_cam1)
+ax = tm.plot_frames_in("Cam0", s=0.5)
+# ax.set_xlim((-0.25, 0.75))
+# ax.set_ylim((-0.5, 0.5))
+# ax.set_zlim((0.0, 1.0))
+plt.show()
+
--- ransac_E.py
+++ ransac_E.py
@@ -2,29 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random 
 from sympy import *
+import cv2 
 
-class Problem:
-    def __init__(self,path_correnspondences,paths_imgs,K_path):
-        self.correspondences = np.load(path_correnspondences)
-        self.imgs = []
-        for img_path in paths_imgs:
-            self.imgs.append(plt.imread(img_path))
-        self.K = np.load(K_path)
-        self.K1 = self.K['K1']
-        self.K2 = self.K['K2']
+class Essential_Optimizer:
+    def __init__(self,pts1,pts2):
 
-        self.pts1 = self.correspondences['pts1']
-        self.pts2 = self.correspondences['pts2']
-        self.pts1 = np.hstack((self.pts1,np.ones_like(self.pts1[:,0]).reshape((-1,1))))
-        self.pts2 = np.hstack((self.pts2,np.ones_like(self.pts2[:,0]).reshape((-1,1))))
-        self.pts1_raw = self.pts1
-        self.pts2_raw = self.pts2
+        self.pts1 = pts1.T
+        self.pts2 = pts2.T
+        self.num_points = self.pts1.shape[0]
 
-    def compute_system(self,num_points = -1):
+    def compute_system(self):
         
         pts1 = self.pts1
         pts2 = self.pts2
-        self.num_points = pts1.shape[0]
+
         self.A = np.zeros((self.num_points,8))
         self.A[:,0] = np.multiply(pts2[:,0],pts1[:,0])
         self.A[:,1] = np.multiply(pts2[:,0],pts1[:,1])
@@ -36,30 +27,8 @@ class Problem:
         self.A[:,7] = pts1[:,1]
         self.b = -np.ones_like(pts2[:,0])
         self.all_idxs = list(range(self.num_points))
-    def normalize_points(self):
-        self.avg_1 = np.mean(self.pts1,axis=0)
-        self.avg_2 = np.mean(self.pts2,axis=0)
 
-        self.all_avg = 0.5*self.avg_1  + 0.5*self.avg_2
-
-        self.dist_mean_1 = np.mean(np.linalg.norm(self.pts1-self.all_avg,axis=1),axis=0)
-        self.dist_mean_2 = np.mean(np.linalg.norm(self.pts2-self.all_avg,axis=1),axis=0)
-        
-        s_1 = np.sqrt(2)/(self.dist_mean_1)
-        s_2 = np.sqrt(2)/(self.dist_mean_2)
-
-        #  make points homogeneous
-        self.pts1 = np.hstack((self.pts1,np.ones_like(self.pts1[:,0]).reshape((-1,1))))
-        self.pts2 = np.hstack((self.pts2,np.ones_like(self.pts2[:,0]).reshape((-1,1))))
-
-        #perform normalizing 
-        self.T_1 = np.array([[s_1,0,-s_1*self.all_avg[0]],[0,s_1,-s_1*self.all_avg[1]],[0,0,1.0]])
-        self.T_2 = np.array([[s_2,0,-s_2*self.all_avg[0]],[0,s_2,-s_2*self.all_avg[1]],[0,0,1.0]])
-    
-        # self.pts1 = (self.T_1.dot(self.pts1.T)).T
-        # self.pts2 = (self.T_2.dot(self.pts2.T)).T
-
-    def compute_F_RANSAC_8(self):
+    def compute_E_RANSAC_8(self):
         random.shuffle(self.all_idxs)
         rand_idxs = self.all_idxs[:8]
         self.local_A = self.A[rand_idxs,:]
@@ -68,35 +37,6 @@ class Problem:
         solution = np.linalg.lstsq(self.local_A,self.local_b)
         solution_vector = np.append(solution[0],[1.0])
         self.F = solution_vector.reshape((3,3))
-
-
-    def compute_F_RANSAC_7(self):
-        random.shuffle(self.all_idxs)
-        rand_idxs = self.all_idxs[:7]
-        self.local_A = self.A[rand_idxs,:]
-        self.local_b = self.b[rand_idxs]
-
-        self.local_A = np.hstack((self.local_A,-self.local_b.reshape((-1,1))))
-
-        u, s, vh = np.linalg.svd(self.local_A)
-
-        f1 = vh[-2]
-        f2 = vh[-1]
-
-        F1 = Matrix(f1.reshape((3,3)))
-        F2 = Matrix(f2.reshape((3,3)))
-        alpha = symbols("alpha")
-
-        F_all = F1*alpha + (1-alpha)*F2
-        polynomial = F_all.det()
-
-        # derive the equation from slide 21, L11
-        coeff = [polynomial.coeff(alpha,3),polynomial.coeff(alpha,2),polynomial.coeff(alpha,1),polynomial.coeff(alpha)]
-        solutions = np.roots(coeff)
-        solutions_img = [solution.imag for solution in  solutions ]
-        best_solution_idx = np.argmin(solutions_img)
-        self.F = solutions[best_solution_idx].real*(f1.reshape((3,3))) + (1-solutions[best_solution_idx].real)*(f2.reshape((3,3)))
-
 
 
     def compute_inliers(self,tol):
@@ -129,45 +69,24 @@ class Problem:
                 self.best_F = self.F
                 print(self.best_inliers)
             self.inliers.append(self.best_inliers)
-
-    def compute_E(self):
-        self.E = self.K2.T.dot(self.F.dot(self.K1))
-        print("E is : ")
-        print(self.E)
-
-    def show_epipolar(self):
-        f, (ax1,ax2) = plt.subplots(nrows=1,ncols=2)
-        ax1.imshow(self.imgs[0])
-        
-        ax2.imshow(self.imgs[1])
-        x_s = np.linspace(0,self.imgs[1].shape[1],100)
-
-        num_samples = 10
-        for i in range(num_samples):
-            sample_idx = random.randint(0,self.num_points)
-            ax1.scatter(self.pts1_raw[sample_idx,0],self.pts1_raw[sample_idx,1],color='red',s=5)
-
-            l = self.best_F.dot(self.pts1[sample_idx,:])
-            y_s = (-np.multiply(x_s,l[0]) - np.ones_like(x_s)*l[-1])/l[1]
-            ax2.plot(x_s,y_s)
-
-        ax2.set_xlim((0,self.imgs[1].shape[1]))
-        ax2.set_ylim((self.imgs[1].shape[0],0))
-
-        # l = self.F.dot(self.pts2[0,:])
-        # y_s = -np.multiply(x_s,l[0]) - np.ones_like(x_s)*l[-1]*self.pts2[0,-1]
-        # ax2.plot(x_s,y_s)
-
-        plt.show()
-
-        plt.plot(self.inliers)
-        plt.xlabel('Iterations')
-        plt.ylabel('Inlier Ratio')
-        plt.grid()
-        plt.show()
+    
+    def full_lstsq(self):
+        solution = np.linalg.lstsq(self.A,self.b)
+        solution_vector = np.append(solution[0],[1.0])
+        self.E = solution_vector.reshape((3,3))
+    
+    def extract_motion(self):
+        w,u, vt = cv2.SVDecomp(self.E)
+        if np.linalg.det(u) < 0:
+            u *= -1.0
+        if np.linalg.det(vt) < 0:
+            vt *= -1.0 
+        W = np.array([[0,-1,0],[1,0,0],[0,0,1]])
+        self.R = u@W@vt 
+        self.t = u[:,2]
 
 
-train = Problem('data/q1b/toytrain/toytrain_corresp_raw.npz',['data/q1b/toytrain/image_1.jpg','data/q1b/toytrain/image_2.jpg'],'data/q1b/toytrain/intrinsic_matrices_toytrain.npz')
-train.compute_system()
-train.ransac_loop(1.2,10000,method='7')
-train.show_epipolar()
+# train = Get_E('data/q1b/toytrain/toytrain_corresp_raw.npz',['data/q1b/toytrain/image_1.jpg','data/q1b/toytrain/image_2.jpg'],'data/q1b/toytrain/intrinsic_matrices_toytrain.npz')
+# train.compute_system()
+# train.ransac_loop(1.2,10000,method='7')
+# train.show_epipolar()
